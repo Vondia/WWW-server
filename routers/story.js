d0e58cd0c42e6b766999b8323b47e1b2f2354ac4
Vondia <p.dronk@hotmail.com>
2021-04-12 12:29:35 +0200

    build a working endpoint for creating new stories

--- routers/story.js
+++ routers/story.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const auth = require("../auth/middleware");
 const Story = require("../models").story;
 const Answer = require("../models").answer;
 
@@ -21,5 +22,66 @@ router.get("/:storyId", async (req, res, next) => {
     next(e);
   }
 });
+
+router.post("/", auth, async (req, res) => {
+  const { name, storySentence, preziUrl, imageUrl, question } = req.body;
+
+  if (!name) {
+    return res.status(400).send({ message: "A story must have a name" });
+  }
+
+  if (!storySentence) {
+    return res
+      .status(400)
+      .send({ message: "A story must have an engaging sentance" });
+  }
+
+  if (!preziUrl) {
+    return res.status(400).send({ message: "A story must have a prezi Url" });
+  }
+
+  if (!imageUrl) {
+    return res.status(400).send({ message: "A story must have an image Url" });
+  }
+
+  if (!question) {
+    return res.status(400).send({ message: "A story must have a question" });
+  }
+
+  const newStoryDone = await Story.create({
+    name,
+    storySentence,
+    preziUrl,
+    imageUrl,
+    question,
+  });
+
+  return res.status(201).send({ message: "Story created", newStoryDone });
+});
+
+// router.post("/tasks", async (req, res) => {
+//   const { name, status, doctorId, userId } = req.body;
+//   if (!name || !userId) {
+//     return res.status(400).send("Please try again");
+//   }
+
+//   try {
+//     const newTask = await Task.create({
+//       name,
+//       userId,
+//       doctorId,
+//       status,
+//     });
+//     //   delete newUser.dataValues["password"]; // don't send back the password hash
+//     //   const token = toJWT({ userId: newUser.id });
+//     res.status(201).json({ ...newTask.dataValues });
+//   } catch (error) {
+//     if (error.name === "SequelizeUniqueConstraintError") {
+//       return res.status(400).send({ message: "There is an error" });
+//     }
+
+//     return res.status(400).send({ message: "you added a task" });
+//   }
+// });
 
 module.exports = router;
